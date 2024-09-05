ca420bbd6c3682f6fe0088387265c112642a1857
FahadulHoq <fahadulhoq@gmail.com>
2024-09-05 15:44:53 +0600

    layout

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Videos from '../Videos';
+
+const Home = ({children}) => {
+    return (
+        <div>
+            <Videos />
+        </div>
+    );
+};
+
+export default Home;
