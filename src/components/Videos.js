256af44f6900762d5946e7a24f79d4d2b6c5e8cb
FahadulHoq <fahadulhoq@gmail.com>
2024-09-10 17:40:29 +0600

    infinity scroll

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,23 +1,41 @@
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { Link } from "react-router-dom";
-import classes from "../styles/Videos.module.css";
+import useVideoList from "../hooks/useVideoList";
 import Video from "./Video";
-import useVideosList from "../hooks/useVideosList";
 
 export default function Videos() {
-  const {loading,error,videos} = useVideosList();
+  const [page, setPage] = useState(1);
+  const { loading, error, videos, hasMore } = useVideoList(page);
+
   return (
-    <div className={classes.videos}>
-      {
-        videos.length > 0 &&
-        videos.map((video)=> (
-          <Link to="/quiz" key={video.youtubeID}>
-            <Video
+    <div>
+      {videos.length > 0 && (
+        <InfiniteScroll
+          dataLength={videos.length}
+          hasMore={hasMore}
+          next={() => setPage(page + 8)}
+        >
+          {videos.map((video) =>
+            video.noq > 0 ? (
+              <Link to="/quiz" key={video.youtubeID}>
+                <Video
                   title={video.title}
                   id={video.youtubeID}
                   noq={video.noq}
-            />
-          </Link>
-        ))}
+                />
+              </Link>
+            ) : (
+              <Video
+                key={video.youtubeID} // Add the key prop here
+                title={video.title}
+                id={video.youtubeID}
+                noq={video.noq}
+              />
+            )
+          )}
+        </InfiniteScroll>
+      )}
       {!loading && videos.length === 0 && <div>No data found!</div>}
       {error && <div>There was an error!</div>}
       {loading && <div>Loading...</div>}
